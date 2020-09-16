be7bd09928e9b753445e4a3e3c59494a2f096a66
Sylvester Yue <sylvesteryue2301@gmail.com>
2020-09-16 03:00:18 -0700

    added validators for text fields in add records page

--- lib/Pages/add_records_page.dart
+++ lib/Pages/add_records_page.dart
@@ -1,24 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class AddRecordsPage extends StatelessWidget {
-  final formKey = GlobalKey<FormState>();
+class AddRecordsPage extends StatefulWidget {
+  @override
+  _AddRecordsPageState createState() => _AddRecordsPageState();
+}
 
-  final friendTextField = TextFormField();
+class _AddRecordsPageState extends State<AddRecordsPage> {
+  final _formKey = GlobalKey<FormState>();
 
-  final moneyTextField = TextFormField(
-    inputFormatters: <TextInputFormatter>[
-      WhitelistingTextInputFormatter.digitsOnly,
-      FilteringTextInputFormatter.singleLineFormatter
-    ],
-  );
+  String friend = '';
+  String money = '';
+  String description = '';
 
-  final descriptionTextField = TextFormField(
-    maxLines: 8,
-  );
+  String moneyValidator(String value) {
+    Pattern pattern = r"^[1-9][0-9]*(\.[0-9]{1,2})?$";
+    RegExp regex = new RegExp(pattern);
+
+    if (value.length == 0) {
+      return 'Please enter a value';
+    }
+
+    if (!regex.hasMatch(value)) {
+      return 'Money value is not in valid format';
+    } else {
+      return null;
+    }
+  }
+
+  String descriptionValidator(String value) {
+    if (value.length == 0) {
+      return 'Please enter a description';
+    } else {
+      return null;
+    }
+  }
+
+  // void searchUser(String searchText) {
+
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final friendTextField = TextFormField();
+
+    final moneyTextField = TextFormField(
+        validator: moneyValidator,
+        onChanged: (val) => setState(() => money = val),
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.allow(RegExp(r"^[0-9.]*")),
+          FilteringTextInputFormatter.singleLineFormatter
+        ],
+        keyboardType: TextInputType.numberWithOptions(decimal: true));
+
+    final descriptionTextField = TextFormField(
+        maxLines: 8,
+        validator: descriptionValidator,
+        onChanged: (val) => setState(() => description = val));
+
+    final submitButton = OutlineButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        onPressed: () {},
+        child: Text("Submit", textAlign: TextAlign.center));
+
     return Scaffold(
       appBar: AppBar(title: Text("Add Record")),
       body: Container(
@@ -26,14 +70,15 @@ class AddRecordsPage extends StatelessWidget {
         child: Padding(
             padding: const EdgeInsets.all(36.0),
             child: Form(
-                key: formKey,
+                key: _formKey,
                 child: ListView(shrinkWrap: true,
                     // crossAxisAlignment: CrossAxisAlignment.center,
                     // mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       friendTextField,
                       moneyTextField,
-                      descriptionTextField
+                      descriptionTextField,
+                      submitButton
                     ]))),
       ),
     );
